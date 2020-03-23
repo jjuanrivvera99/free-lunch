18e166f41a941ba2c99ea014c1aa9fb6b7b5eaa6
Juan Felipe Rivera González <juan.rivera@alegra.com>
2020-03-22 21:05:44 -0500

    Herouku database setup

--- config/database.php
+++ config/database.php
@@ -63,11 +63,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $DATABASE_URL['host']),
+            'port' => env('DB_PORT', $DATABASE_URL['port']),
+            'database' => env('DB_DATABASE',  ltrim($DATABASE_URL['path'], '/')),
+            'username' => env('DB_USERNAME', $DATABASE_URL['user']),
+            'password' => env('DB_PASSWORD', $DATABASE_URL['pass']),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
