c9c4bb17887931bef9f3d5c1807e7c52900a6118
Juan Felipe Rivera González <juan.rivera@alegra.com>
2020-03-24 14:05:58 -0500

    Stupid tab

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', '	jjuanrivvera@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'jjuanrivvera@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Free Lunch'),
     ],
 
